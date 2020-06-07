c04e91066256dae478330b614db360243e2677de
Chanpreet Singh <chanpreet1999@gmail.com>
2020-06-07 23:23:58 +0530

    MAJOR CHANGES: Added plalist SS options

--- fair/activity/pep.bat
+++ fair/activity/pep.bat
@@ -1,3 +1,8 @@
+self search
 node youtube -s "yuvraj singh 6 sixes" 30
-node youtube -l "https://www.youtube.com/watch?v=EFOCcLdhG7M" 20
-node youtube -l "https://www.youtube.com/watch?v=1SvraBE390g" 20
+
+scars to your beautiful
+node youtube -l "https://www.youtube.com/watch?v=MWASeaYuHZo" 20
+
+self curated small playlist
+node youtube -p "https://www.youtube.com/playlist?list=PLQVkTl3ccjbzbJIFzxIZy3WlRyoHqRZCp" 30
--- fair/activity/youtube.js
+++ fair/activity/youtube.js
@@ -5,6 +5,8 @@ const rimraf = require('rimraf');
 
 let image = [];
 let timer;
+let curCountg = 1;
+const doc = new docx.Document();
 
 (async function () {
     try {
@@ -29,6 +31,8 @@ let timer;
         let cmd = process.argv[2];
         timer = process.argv[4];
         timer = parseInt(timer);
+
+        //-----------------------------MENU-------------------------------------//
         switch (cmd) {
 
             case '-l': console.log('Screenshot using link');
@@ -51,50 +55,104 @@ let timer;
         //-----------------------------------------------------------//
 
         browser.close();
-        rimraf(dir, function(err){
-            if(err == null)
+
+        //finally write to the word document
+        docx.Packer.toBuffer(doc).then(async function (buffer) {
+            fs.writeFile("My_Youtube_Screenshots.docx", buffer, function () {
+                console.log('Written to file');
+            });
+        });
+
+        //remove the directory
+        rimraf(dir, function (err) {
+            if (err == null)
                 console.log('Folder removed');
-            else{
+            else {
                 console.log('error wile deleting folder');
                 console.log(err);
-            }    
+            }
         });
+
     } //try ends
     catch (err) {
+        console.log('error in driver function');
         console.log(err);
     }
 })();
 
 
 async function ssLinks(browser, tab, link) {
-    await tab.goto(link, {
-        waitUntil: "networkidle0"
-    });
+    try {
+        await tab.goto(link, {
+            waitUntil: "networkidle0"
+        });
 
-    await afterVidOpens(browser, tab);
+        await afterVidOpens(browser, tab);
+
+    } //try ends 
+    catch (err) {
+        console.log('Error while taking ss from link');
+        console.log(err);
+    }
 }
 
 async function ssSearch(browser, tab, search) {
+    try {
+        await tab.goto("https://youtube.com/", {
+            waitUntil: "networkidle0"
+        });
 
-    await tab.goto("https://youtube.com/", {
-        waitUntil: "networkidle0"
-    });
+        //search for the vid
+        await tab.waitForSelector("#container input#search");
+        await tab.type("#container input#search", search, { delay: 70 });
+        await Promise.all([tab.keyboard.press("Enter"), tab.waitForNavigation({ waitUntil: "networkidle2" })]);
+        await tab.waitForSelector("#contents a#video-title")
 
-    //search for the vid
-    await tab.waitForSelector("#container input#search");
-    await tab.type("#container input#search", search, { delay: 70 });
-    await Promise.all([tab.keyboard.press("Enter"), tab.waitForNavigation({ waitUntil: "networkidle2" })]);
-    await tab.waitForSelector("#contents a#video-title")
+        //get the 1st result
+        let firstRes = await tab.$("#contents a#video-title")
+        await Promise.all([firstRes.click(), tab.waitForNavigation({ waitUntil: "networkidle2" })])
+        await afterVidOpens(browser, tab);
+    
+    }//try ends
+    catch (err) {
+        console.log('Error while taking ss fronm Search element');
+        console.log(err);
+    }
+}
 
-    //get the 1st result
-    let firstRes = await tab.$("#contents a#video-title")
-    await Promise.all([firstRes.click(), tab.waitForNavigation({ waitUntil: "networkidle2" })])
-    await afterVidOpens(browser, tab);
+async function ssPlaylist(browser, tab, playlistLink) {
+    try {
+        await tab.goto(playlistLink, { waitUntil: "networkidle0" });
+
+        //get the list of all vids
+        const vidList = await tab.$$('a.yt-simple-endpoint.style-scope.ytd-playlist-video-renderer');
+
+        //get  href of every element
+        let href = [];
+        //   let curUrl = tab.url();
+        for (let ele of vidList) {
+            let curHref = await tab.evaluate(el => el.getAttribute("href"), ele);
+            href.push('https://www.youtube.com' + curHref);
+        }
+
+        for (let i = 0; i < href.length; i++) {
+            await tab.goto(href[i], {
+                waitUntil: "networkidle2",
+                timeout : 60 * 1000
+            });
+            await afterVidOpens(browser, tab);
+            curCountg = image.length + 1;
+        }
+
+    }//try ends
+    catch (err) {
+        console.log('Error while taking ss of PLAYLIST');
+        console.log(err);
+    }
 }
 
 async function afterVidOpens(browser, tab) {
     try {
-
         await tab.waitForSelector("button.ytp-fullscreen-button.ytp-button");   //fullscreen
         await tab.click("button.ytp-fullscreen-button.ytp-button");
 
@@ -136,7 +194,8 @@ async function afterVidOpens(browser, tab) {
         await takeScreenshots(browser, tab, curVal, endVal);
 
 
-    } catch (err) {
+    } // try ends
+     catch (err) {
         console.log('Error encountered after opening the vid');
         console.log(err);
     }
@@ -144,11 +203,11 @@ async function afterVidOpens(browser, tab) {
 
 async function takeScreenshots(browser, tab, curVal, endVal) {
     try {
-
         let waitingTimeP;
-        const doc = new docx.Document();
 
-        for (let i = 1; curVal < endVal; i++) {
+        for (let i = curCountg; curVal < endVal; i++) {
+            //wait for ads to be cleared
+            await tab.waitForSelector(".ytp-ad-persistent-progress-bar-container", { hidden: true });
             await tab.screenshot({
                 path: `Screenshots\\${i}.jpeg`,
                 type: "jpeg",
@@ -169,14 +228,10 @@ async function takeScreenshots(browser, tab, curVal, endVal) {
 
         console.log("No of ss taken :" + image.length);
 
-        //finally write to the word document
-        docx.Packer.toBuffer(doc).then(async function(buffer) {
-            fs.writeFile("My_Youtube_Screenshots.docx", buffer, function(){
-                console.log('Written to file');
-            });
-        });
 
-    } catch (err) {
+
+    } // try ends 
+    catch (err) {
         console.log('Error while taking ss ');
     }
 }
